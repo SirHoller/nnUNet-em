9268dc335cf7e60f6c948784020970a3b762c8be
Alexander Holler Rodriguez <aholler@uoc.edu>
2024-12-09 23:14:38 +0100

    Optimizar archivos JSON y agregar script de entrenamiento para el conjunto de datos de nuevas lesiones

---
+++ scripts/E_train.bat
@@ -0,0 +1,27 @@
+@echo off
+REM Paths:
+set nnUNet_raw=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_raw_data
+set nnUNet_preprocessed=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_preprocessed_data
+set nnUNet_results=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_results
+
+REM Preprocess dataset
+nnUNetv2_plan_and_preprocess -d 100 --verify_dataset_integrity
+
+REM Train folds (we could also use "all" to train all splits):
+set dataset=100
+set conf=3d_fullres
+set trainer=nnUNetTrainerCustomOversamplingEarlyStopping
+
+nnUNetv2_train %dataset% %conf% 0 -device cuda -tr %trainer% --npz
+nnUNetv2_train %dataset% %conf% 1 -device cuda -tr %trainer% --npz
+nnUNetv2_train %dataset% %conf% 2 -device cuda -tr %trainer% --npz
+nnUNetv2_train %dataset% %conf% 3 -device cuda -tr %trainer% --npz
+nnUNetv2_train %dataset% %conf% 4 -device cuda -tr %trainer% --npz
+
+REM Find best configuration:
+nnUNetv2_find_best_configuration %dataset% -c %conf% -tr %trainer%
+
+REM Predict
+set INPUT_FOLDER=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_raw_data\Dataset100_NewLesions\imagesTs
+set OUTPUT_FOLDER=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_test_results\Dataset100_NewLesions\nnUNetTrainerCustomOversamplingEarlyStopping__nnUNetPlans__3d_fullres
+nnUNetv2_predict -d %dataset% -i %INPUT_FOLDER% -o %OUTPUT_FOLDER% -f 0 1 2 3 4 -tr %trainer% -c %conf% -p nnUNetPlans
--- scripts/E_train.sh
+++ scripts/E_train.sh
@@ -1,7 +1,7 @@
 # Paths:
-# export nnUNet_raw="./data/nnUNet_raw_data"
-# export nnUNet_preprocessed="./data/nnUNet_preprocessed_data"
-# export nnUNet_results="./nnUNet_results"
+export nnUNet_raw="C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_raw_data"
+export nnUNet_preprocessed="C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_preprocessed_data"
+export nnUNet_results="C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_results"
 
 # Preprocess dataset
 nnUNetv2_plan_and_preprocess -d 100 --verify_dataset_integrity
@@ -9,7 +9,7 @@ nnUNetv2_plan_and_preprocess -d 100 --verify_dataset_integrity
 # Train folds (we could also use "all" to train all splits):
 dataset=100
 conf=3d_fullres
-trainer=nnUNetTrainerWithEarlyStopping
+trainer=nnUNetTrainerCustomOversamplingEarlyStopping
 nnUNetv2_train $dataset $conf 0 -device cuda -tr $trainer --npz
 nnUNetv2_train $dataset $conf 1 -device cuda -tr $trainer --npz
 nnUNetv2_train $dataset $conf 2 -device cuda -tr $trainer --npz
@@ -20,6 +20,6 @@ nnUNetv2_train $dataset $conf 4 -device cuda -tr $trainer --npz
 nnUNetv2_find_best_configuration $dataset -c $conf -tr $trainer
 
 # Predict
-INPUT_FOLDER=/Users/castiler/TFM/nnUNet-em/nnUNet_raw_data/Dataset100_NewLesions/imagesTs
-OUTPUT_FOLDER=/Users/castiler/TFM/nnUNet-em/nnUNet_test_results/Dataset100_NewLesions/nnUNetTrainerWithEarlyStopping__nnUNetPlans__3d_fullres
+INPUT_FOLDER=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_raw_data\Dataset100_NewLesions\imagesTs
+OUTPUT_FOLDER=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_test_results\Dataset100_NewLesions\nnUNetTrainerCustomOversamplingEarlyStopping__nnUNetPlans__3d_fullres
 nnUNetv2_predict -d $dataset -i $INPUT_FOLDER -o $OUTPUT_FOLDER -f  0 1 2 3 4 -tr $trainer -c $conf -p nnUNetPlans

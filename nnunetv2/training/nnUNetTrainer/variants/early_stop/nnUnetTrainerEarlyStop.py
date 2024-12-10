4ab1a32a068092def546c915e7bb4a00c837b135
Alexander Holler Rodriguez <aholler@uoc.edu>
2024-12-10 19:34:10 +0100

    Refactor el bucle de entrenamiento y validación para mejorar la legibilidad y la gestión de lotes

--- nnunetv2/training/nnUNetTrainer/variants/early_stop/nnUnetTrainerEarlyStop.py
+++ nnunetv2/training/nnUNetTrainer/variants/early_stop/nnUnetTrainerEarlyStop.py
@@ -79,12 +79,18 @@ class nnUNetTrainerEarlyStopping(nnUNetTrainer):
         self.on_train_start()
         for epoch in range(self.current_epoch, self.num_epochs):
             self.on_epoch_start()
-            train_outputs = [self.train_step(next(self.dataloader_train)) for _ in range(self.num_iterations_per_epoch)]
+            
+            self.on_train_epoch_start()
+            train_outputs = []
+            for batch_id in range(self.num_iterations_per_epoch):
+                train_outputs.append(self.train_step(next(self.dataloader_train)))
             self.on_train_epoch_end(train_outputs)
 
             with torch.no_grad():
                 self.on_validation_epoch_start()
-                val_outputs = [self.validation_step(next(self.dataloader_val)) for _ in range(self.num_val_iterations_per_epoch)]
+                val_outputs = []
+                for batch_id in range(self.num_val_iterations_per_epoch):
+                    val_outputs.append(self.validation_step(next(self.dataloader_val)))
                 self.on_validation_epoch_end(val_outputs)
 
             self.on_epoch_end()

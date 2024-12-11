480a08bb20bfc84c8aa41251a0da708cd75ea0ae
Alexander Holler Rodriguez <aholler@uoc.edu>
2024-12-11 19:16:22 +0100

    Eliminar archivos de configuración obsoletos y actualizar resultados de inferencia para el entrenamiento de nnUNet con sobre-muestreo personalizado.

--- scripts/E_train.bat
+++ scripts/E_train.bat
@@ -5,7 +5,7 @@ set nnUNet_preprocessed=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_preprocessed_data
 set nnUNet_results=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_results
 
 REM Preprocess dataset
-nnUNetv2_plan_and_preprocess -d 100 --verify_dataset_integrity
+@REM nnUNetv2_plan_and_preprocess -d 100 --verify_dataset_integrity
 
 REM Train folds (we could also use "all" to train all splits):
 set dataset=100
@@ -16,10 +16,10 @@ set trainer=nnUNetTrainerCustomOversamplingEarlyStopping
 @REM nnUNetv2_train %dataset% %conf% 1 -device cuda -tr %trainer% --npz --val_best=1
 @REM nnUNetv2_train %dataset% %conf% 2 -device cuda -tr %trainer% --npz --val_best=1
 @REM nnUNetv2_train %dataset% %conf% 3 -device cuda -tr %trainer% --npz --val_best=1
-nnUNetv2_train %dataset% %conf% 4 -device cuda -tr %trainer% --npz
+@REM nnUNetv2_train %dataset% %conf% 4 -device cuda -tr %trainer% --npz
 
-REM Find best configuration:
-nnUNetv2_find_best_configuration %dataset% -c %conf% -tr %trainer%
+@REM REM Find best configuration:
+@REM nnUNetv2_find_best_configuration %dataset% -c %conf% -tr %trainer%
 
 REM Predict
 set INPUT_FOLDER=C:\Users\mrtwe\TFM\nnUNet-em\nnUNet_raw_data\Dataset100_NewLesions\imagesTs
